628058507fd33fcb7a349fc5168fc49450450843
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2025-06-20 11:52:10 +0200

    html skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
+import '@picocss/pico'
 import { useState } from 'react'
 import qmatesLogo from './assets/qmates.svg'
 import './App.css'
-import '@picocss/pico'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <img src={qmatesLogo} className="logo qmates" alt="QMates logo" />
-      </div>
-      <h1>Hexagonal Architecture</h1>
-      <div className="card">
-        <button onClick={handleClick}>count is {count}</button>
+      <header className='top-bar'>
+        <nav className="container">
+          <ul>
+            <li><img src={qmatesLogo} className="logo" alt="QMates logo" /></li>
+          </ul>
+          <ul>
+            <li></li>
+          </ul>
+        </nav>
+      </header>
+      <div className='container page'>
+        <article className="wall">
+          <header>
+            <h3>Hexagonal Architecture</h3>
+          </header>
+          <div>
+            <button onClick={handleClick}>count is {count}</button>
+          </div>
+        </article>
       </div>
     </>
   )
