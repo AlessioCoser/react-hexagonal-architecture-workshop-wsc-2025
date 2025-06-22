f9ecb67230646df66a6f11d7825d6624f50f610f
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2025-06-22 22:49:13 +0200

    add tags as badges

--- src/Post/PostsList/PostItem.tsx
+++ src/Post/PostsList/PostItem.tsx
@@ -1,10 +1,17 @@
 import type { Post } from '../Post.ts'
+import { Badge } from '../../helpers/Badge/Badge.tsx'
 
 export function PostItem({ post }: { post: Post }) {
   return (
     <article>
-      {post.title && <strong>{post.title}<br /></strong>}
-      {post.text}
+      {post.title && (
+        <strong>
+          {post.title}
+          <br />
+        </strong>
+      )}
+      <div>{post.text}</div>
+      {post.tags?.map((tag, index) => <Badge key={index}>{tag}</Badge>)}
       <footer>
         <small>{footer()}</small>
       </footer>
